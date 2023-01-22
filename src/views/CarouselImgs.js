df7c5f6e56dd038c58dd71d0e6e19aaffd0db63d
AmbarPHP <perlamarina.franco@gmail.com>
2023-01-21 19:57:42 -0600

    Add Carrusel

--- src/views/CarouselImgs.js
+++ src/views/CarouselImgs.js
@@ -1,19 +1,23 @@
 import Carousel from 'react-bootstrap/Carousel';
-import img1 from "../assets/img/dental5.jpg";
-import img2 from "../assets/img/dental4.jpg"
-import img3 from "../assets/img/dental5.jpg"
+import img1 from "../assets/img/carrusel/carrusel1.png";
+import img2 from "../assets/img/carrusel/carrusel2.png";
+import img3 from "../assets/img/carrusel/carrusel3.png";
+import img4 from "../assets/img/carrusel/carrusel4.png";
 
 function CarouselImgs() {
   return (
-    <Carousel>
+    <section className="section">
+    <div className="row">
+				  <div className="col-md-12 ">
+          <Carousel>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          className="d-block w-100 "
           src={img1}
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <h1 className="text-uppercase" >Nos preocupamos por tu salud</h1>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
@@ -25,7 +29,7 @@ function CarouselImgs() {
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
+          <h1  className="text-uppercase"> Trato digno y accesible para todos</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
@@ -37,13 +41,31 @@ function CarouselImgs() {
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h1  className="text-uppercase">Profesionalismo y dedicacion nos caracterizan</h1>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={img4}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h1  className="text-uppercase">Obten la dentadura mas hermosa</h1>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
+          </div>
+    </div>
+    </section>      
+   
   );
 }
 
