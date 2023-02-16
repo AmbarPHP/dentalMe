da4c9ea515dacd140c696a1d9e4486c8760208a4
AmbarPHP <perlamarina.franco@gmail.com>
2023-02-16 14:51:01 -0600

    Add title

--- src/views/Ubicanos/Ubicanos.js
+++ src/views/Ubicanos/Ubicanos.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import mapa from "../../assets/img/mapa.png"
+import { Col } from 'react-bootstrap';
+import mapa from "../../assets/img/mapa.png";
+
+
 export default function Ubicanos() {
   return (
     <div >
-   
+       <Col className='d-flex justify-content-center align-items-center mt-5' lg={12} >
+                    <h3 className="text-primary  bold mb-5"
+                        style={{ fontSize: 3 + "rem" }} >Nos ubicamos aqui</h3>
+                </Col>
       <img src={mapa}/>
     </div>
   )
