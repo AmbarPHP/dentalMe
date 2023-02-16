90e0b5ae8cd4b357766f1c467ee8210814be77c3
AmbarPHP <perlamarina.franco@gmail.com>
2023-02-16 14:48:48 -0600

    Add some style to Testimonials

--- src/views/Testimonials/Testimonial.js
+++ src/views/Testimonials/Testimonial.js
@@ -1,44 +1,57 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
+
 export default function Testimonial() {
+    const data = [{
+        testimonial: "Yo quisera agradecerle al personal de la Dra ,incluyendo al laboratorista, por facilitar mi cirugia el dia 9 de Enero",
+        by: "Martha Solana",
+        picture: "doc3",
+        title: "Profesor de la universidad UAN"
+    },
+    {
+        testimonial: "Yo admiro su dedicacion,su trabajo es inpecable dia  12 de Enero",
+        by: "Bryan Bunch",
+        picture: "doc1",
+        title: "Paciente"
+    }
+        ,
+    {
+        testimonial: "Me gusta mucho su trabajo, son muy profesionales",
+        by: "Issac Levi",
+        picture: "doc2",
+        title: "Contador de Nissan"
+    }]
     return (
-        <Container className="container" style={{height:300+"px", fontSize:20+"px"}}>
-           <Col className='d-flex justify-content-center align-items-center mt-5' lg={12} >
-                        <h3 className="text-primary  bold mb-5" style={{ fontSize: 3 + "rem" }} >Testimoniales</h3>
-                    </Col>
-            <Row className="offset-top-66">
-     
+        <Container className="container" style={{ height: "auto", fontSize: 20 + "px" }}>
+            <Col className='d-flex justify-content-center align-items-center mt-5' lg={12} >
+                <h3 className="text-primary  bold mb-5" style={{ fontSize: 3 + "rem" }} >Testimoniales</h3>
+            </Col>
+            <Row className="mt-5 mb-3">
 
-                <Col lg={6} sm={12} md={12}>
-                <p className="font-italic text-gray-dark">
-                <q>Yo quisera agradecerle al personal de la Dra ,incluyendo al laboratorista, por facilitar mi cirugia el dia 9 de Enero</q>
+                {
+                    data.map((item) => {
+                        return (
+                            <Col lg={6} sm={12} md={12} >
+                                <p className="font-italic text-gray-dark">
+                                    <q className='font-20'>{item.testimonial}</q>
                                 </p>
-                                <img className="rounded-circle quote-img" 
-                                style={{width:"60", height:"60"}} 
-                                src="images/users/user-bryan-green-60x60.jpg" alt="" />
+
+                                <hr className='text-contrast bold'/>
+                                <img
+                                    className="rounded-circle quote-img"
+                                    style={{ width: "30", height: "30" ,borderRadius:"25%" ,borderColor:"#cc5" ,borderStyle:"solid"}}
+                                    src={require('../../assets/img/Doctores/' + item.picture + '.png')} />
                                 <h5 className="quote-author text-capitalize font-weight-bold text-primary">
-                                            <cite className="text-normal">Martha Solana</cite>
-                                        </h5>
-                                        <p className="quote-desc text-capitalize text-gray font-italic">Patient</p>
-                </Col>
-                <Col lg={6} sm={12} md={12}>
-                <p className="font-italic text-gray-dark">
-                                    <q>Yo quisera agradecerle al personal de la Dra ,incluyendo al laboratorista, por facilitar mi cirugia el dia 9 de Enero</q>
-                                </p>
-                                <img className="rounded-circle quote-img" 
-                                style={{width:"60", height:"60"}} 
-                                src="images/users/user-bryan-green-60x60.jpg" alt="" />
-                                <h5 className="quote-author text-capitalize font-weight-bold text-primary">
-                                            <cite className="text-normal">Ever Zamora</cite>
-                                        </h5>
-                                        <p className="quote-desc text-capitalize text-gray font-italic">Paciente</p>
-                </Col>
+                                    <cite className="text-normal">{item.by}</cite>
+                                </h5>
+                                <p className="quote-desc  text-gray font-italic">{item.title}</p>
+                            </Col>
+                        )
+                    })
+                }
 
 
-
-
-                 
             </Row>
         </Container>
     )
