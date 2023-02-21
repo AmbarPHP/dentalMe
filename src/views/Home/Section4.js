0604de8b33f95907fa74d13c487ef33d21f6550b
AmbarPHP <perlamarina.franco@gmail.com>
2023-02-21 11:43:06 -0600

    Add data object  to section 4 , adding a list to map data

--- src/views/Home/Section4.js
+++ src/views/Home/Section4.js
@@ -1,64 +1,59 @@
 import React from 'react'
-import { Container, Row,Col, Button } from 'react-bootstrap';
-import rounded2 from "../../assets/img/carrusel/carrusel2.png"
-import rounded3 from "../../assets/img/carrusel/carrusel3.png"
-import rounded4 from "../../assets/img/carrusel/carrusel4.png"
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
 
 export default function Section4() {
-    
+    const data = [{
+        title_banner: "AboutUs",
+        description:"We hire the best specialists to deliver top-notch diagnostic services for you.",
+        picture: "carrusel4",
+    },
+    {
+        title_banner: "Equipo Moderno",
+        description:"We use the first-class medical equipment for timely diagnostics of various diseases.",
+        picture: "carrusel2",
+    },
+    {
+        title_banner: "Servicio de Emergencias",
+        description: "Our emergency diagnostics services help you get the most accurate diagnosis in a minimal time.",
+        picture: "carrusel3",
+    }
+];
 
 
     return (
 
         <Container name="section4-home" className="content bg-light alpha-4 mb-5" >
             <Row className="mb-5 mt-5 d-flex justify-content-end  flex-row" >
-            <Col  md={6} lg={8} sm= {12} >  
-            <h1 className='text-secondary bold font-3rem' >Nuestros servicios</h1>  </Col>
+                <Col md={6} lg={8} sm={12} >
+                    <h1 className='text-secondary bold font-3rem' >Nuestros servicios</h1>  </Col>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur, voluptatum, atque maxime dignissimos magni laborum illo adipisci perspiciatis, accusamus incidunt laudantium aperiam quidem. Beatae ut illum at praesentium non fuga.</p>
             </Row>
-           
+
             <Row>
-                <Col  md={6} lg={4} >
-                    <Row >
+                {
+                    data.map((item, index) => {
+                        return (
+                       
+                                <Col md={6} lg={4} key={index}>
+                                    <Row >
+                                        <img
+                                    className="rounded-pill"
+                                    style={{ maxWidth: 100 + "%", height: "auto" }}
+                                    src={require('../../assets/img/carrusel/' + item.picture + '.png')} />
+                                    </Row>
+                                    <Row>
+                                        <h4 className="bold text-primary mt-5">{item.title_banner}</h4>
+                                        <p>{item.description}</p>
+                                    </Row>
 
-                        <img className="rounded-pill" alt="avatar1" 
-                        src={rounded2} 
-                        style={{maxWidth:100+"%", height:"auto"}}/>
+                                </Col>
+                            
+                           
+                        )
+                    })
+                }
 
-                    </Row>
-                    <Row>
-                        <h4 className="bold text-primary mt-5">Especialistas Calificados</h4>
-                        <p>We hire the best specialists to deliver top-notch diagnostic services for you.</p>
-                    </Row>
-
-                </Col>
-                <Col  md={6} lg={4} > 
-
-                    <Row >
-
-                    <img className="rounded-pill" alt="avatar1" src={rounded3} style={{maxWidth:100+"%", height:"auto"}}/>
-
-                    </Row>
-                    <Row>
-                        <h4 className="bold text-primary mt-5">Equipo Moderno</h4>
-                        <p>We use the first-class medical equipment for timely diagnostics of various diseases.</p>
-                    </Row>
-                </Col> 
-
-               
-
-                <Col  md={6} lg={4} >
-                    <Row >
-
-                    <img className="rounded-pill" alt="avatar1" src={rounded4} style={{maxWidth:100+"%", height:"auto"}}/>
-
-                    </Row>
-                    <Row>
-                        <h4 className="bold text-primary mt-5">Servicio de Emergencias</h4>
-                        <p>Our emergency diagnostics services help you get the most accurate diagnosis in a minimal time.</p>
-                    </Row>
-
-                </Col> 
 
             </Row>
         </Container>
