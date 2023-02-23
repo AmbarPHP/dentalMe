a7038279523bcd8eccfbc8f85bcb80aeaafa5bb7
AmbarPHP <perlamarina.franco@gmail.com>
2023-02-22 18:16:34 -0600

    Add  menu fixed

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect} from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -11,17 +11,47 @@ import "./Menu.scss";
 
 
 export default function Menu() {
+      const stickyRef = useRef(null);
+     const [sticky, setSticky]=useState(false);
+     const [offset, setOffset]=useState(0);
+     const [variant, setVariant]=useState('');
 
+      useEffect(() => {
+        const handleScroll = () => {
+            
+            const altura_hero=480;
+            if(window.scrollY > altura_hero){
+                        setSticky(true);
+                        console.log("true",window.scrollY)
+                        setOffset(window.scrollY);
+                        setVariant(`position:'fixed ',top:${offset}`);
+                        console.log("variant", variant);
+                    }
+                    else {
+                        setSticky(false);
+                        console.log("false",window.scrollY)
+                    }
+
+        };
+        window.addEventListener('scroll', handleScroll);
+  
+      }, [setSticky,offset]);
+
+      
 
     return (
-        
-            <Navbar  expand="lg" className="menu-container" >
+        // 
+        //className={`${sticky && 'fixed'}`} //"" menu-container
+        //style={{position:'fixed ',top:80}}
+        //style={{position:'fixed ',top:80}} 
+        //5:44 style={{variant}} 
+            <Navbar  expand="lg"   className={`${sticky ?' fixed1 ':'fixed-before'}`} style={{position:'fixed '}}  >
                
                     
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     
                     <Navbar.Collapse id="basic-navbar-nav" className='margin-20'  >
-                        <Nav className='font-20' >
+                        <Nav   className='font-20' >
                             <Nav.Link href="/home"  className='text-light text-uppercase'>Inicio</Nav.Link>
                             <Nav.Link href="/servicios" className='text-light text-uppercase'>Servicios</Nav.Link>
                             <Nav.Link href="/about" className='text-light text-uppercase'>Nuestro equipo</Nav.Link>
