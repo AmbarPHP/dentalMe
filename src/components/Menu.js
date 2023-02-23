bffd46b76c0545866f31f76dbf6c9b5a38daec9c
AmbarPHP <perlamarina.franco@gmail.com>
2023-02-23 11:17:22 -0600

    Add hover effect

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -43,10 +43,10 @@ export default function Menu() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     
                     <Navbar.Collapse id="basic-navbar-nav" className='margin-20'  >
-                        <Nav   className='font-20' >
+                        <Nav   className='font-20 p-1' >
                             <Nav.Link href="/home"  className='text-light text-uppercase'>Inicio</Nav.Link>
                             <Nav.Link href="/servicios" className='text-light text-uppercase'>Servicios</Nav.Link>
-                            <Nav.Link href="/about" className='text-light text-uppercase'>Nuestro equipo</Nav.Link>
+                            <Nav.Link href="/about" className='text-light text-uppercase'>Nuestros Profesionales</Nav.Link>
                             <Nav.Link href="/ubicacion" className='text-light text-uppercase'>Ubicanos</Nav.Link>
                             <Nav.Link href="/testimoniales" className='text-light text-uppercase'>testimoniales</Nav.Link>
 
