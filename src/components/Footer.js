b7a44abf0a69bc25f336c626182007baaa82c870
AmbarPHP <perlamarina.franco@gmail.com>
2023-02-23 22:07:22 -0600

    add icon and aligment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,92 +1,123 @@
 import React from 'react';
 
-import { FaWhatsapp, FaFacebook, FaTwitter, FaInstagram, FaYoutube } from 'react-icons/fa';
+import { FaWhatsapp, FaFacebook, FaTwitter, FaInstagram, FaYoutube, FaMapMarkerAlt } from 'react-icons/fa';
 import { Container, Row, Col } from 'react-bootstrap';
 import logo from "../assets/img/logo.png";
 import "./Footer.scss"
 
 const Footer = () => {
     return (
-        <div className=" bg-primary  pt-3" style={{height:300+"px", fontSize:20+"px"}}>
-         
-            
-                    <Row className=" gb-light gap-y pt-0 mt-0 text-light ">
-                        <div className="col-md-4 ">
-                            <h5 className="bold d-flex justify-content-center mb-0 mt-5">Horarios</h5>
-                            <hr/>
-                                <p className="d-flex justify-content-center mb-0 ">
-                                    Lunes a Viernes 9 a 2 PM y 4 a 8 PM
-                                </p>
-                                <p className="d-flex justify-content-center mb-0 ">
-                                    Sábados 9 a 2 PM y 4 a 8 PM
-                                </p>
-                                <p className="d-flex justify-content-center mb-0 ">
-                                    Domingo Cerrado
-                                </p>
-                                
+        <div className=" bg-primary pt-3" style={{ height: 300 + "px", fontSize: 20 + "px" }}>
+
+
+            <Row className=" gb-light gap-y pt-0 mx-5 text-light ">
+                <div className="col-lg-4">
+                    <h5 className="bold d-flex justify-content-start mb-0 mt-5">Horarios</h5>
+                    <hr />
+                    <div className='row'>
+                        <div d-flex justify-content-start mx-5 >
+                        <p>
+                        Lunes a Viernes 9 a 2 PM y 4 a 8 PM
+                    </p>
+                    <p>
+                        Sábados 9 a 2 PM y 4 a 8 PM
+                    </p>
+                    <p>
+                        Domingo Cerrado
+                    </p>
                         </div>
-                        <div className="col-md-4">
-                        <h5 className="bold d-flex justify-content-center mb-0 mt-5">Citas:</h5>
-                        <hr/>
-                        <div className="d-flex flex-row  justify-content-center mb-0 small text-light pb-3">
-                                
-                                
-                                <a href="https://www.facebook.com/HealthSmileCenter/" 
-                                className="me-4  icon-circle text-primary">
-                                <FaFacebook className='icon-whatsap' />
-                                </a> 
-                                <a href="#" className="me-4 icon-circle  text-primary">
-                                <FaTwitter className='icon-whatsap'/>
-                                </a> 
-                                <a href="#" className="me-4 icon-circle  text-primary">
-                                <FaWhatsapp className='icon-whatsap'/>
-                                </a> 
-                                <a href="#" className=" icon-circle  text-primary">
-                                <FaInstagram className='icon-whatsap'/>
-                                </a>
-                                
-                            </div>
-                            <div className='columna-doble d-flex justify-content-center align-items-center'>
-                                <span className='bold'> 
-                                311 269 7923</span>
-                                
-                            </div>
+
+                    </div>
+                  
+
+                </div>
+                <div className='col-lg-4'>
+                    <h5 className="bold d-flex justify-content-start mb-0 mt-5">Dirección:</h5>
+                    <hr />
+
+                    <div className='row'>
+                    
+                        <Col lg={{span:1, offset:0}} className="p-0">
+                        <span> <a href="https://www.facebook.com/HealthSmileCenter/"
+                                className="  icon-circle text-primary">
+                                <FaMapMarkerAlt className='icon-whatsap' />
+                            </a></span>
+                        </Col>
+                        <Col  lg={{span:11, offset:0}} classname="d-flex justify-content-start my-0">
                             
-                           
-                            
-                        </div>
-                        <div className='col-md-4'>
-                          
-                            <h5 className="bold d-flex justify-content-center mb-0 mt-5">Dirección:</h5>
-                            <hr/>
-                                <p className="d-flex justify-content-center mb-0 mt-3">
-                                Plaza Manglar, Int. 2 
-                                </p>
-                                <p className="d-flex justify-content-center mb-0 ">
-                                Av. de la Cultura #94 Int. 2, 
-                                </p>
-                                <p className="d-flex justify-content-center mb-0 ">
+                            <p >
+                                Plaza Manglar, Int. 2
+                            </p>
+                            <p >
+                                Av. de la Cultura #94 Int. 2,
+                            </p>
+                            <p >
                                 Los Fresnos Oriente, 63190 Tepic, Nay.
-                                </p>
-                              
-                            
-                        </div>
-                        {/* <div className"col-md-3">
-                        <h5 className"bold d-flex justify-content-center mb-0 mt-5">Siguenos:</h5>
-                            
+                            </p>
+                        </Col>
+                    </div>
+
+
+
+
+
+                </div>
+
+                <div className="col-lg-4">
+                    <h5 className="bold d-flex justify-content-start mb-0 mt-5">Citas:</h5>
+
+                    <hr />
+
+                    <div className="d-flex flex-row  justify-content-start mb-0 small text-light pb-3">
+
+
+                        <a href="https://www.facebook.com/HealthSmileCenter/"
+                            className="me-4  icon-circle text-primary">
+                            <FaFacebook className='icon-whatsap' />
+                        </a>
+                        <a href="#" className="me-4 icon-circle  text-primary">
+                            <FaTwitter className='icon-whatsap' />
+                        </a>
+                        <a href="#" className="me-4 icon-circle  text-primary">
+                            <FaWhatsapp className='icon-whatsap' />
+                        </a>
+                        <a href="#" className=" icon-circle  text-primary">
+                            <FaInstagram className='icon-whatsap' />
+                        </a>
+
+                    </div>
+                    <div className='columna-doble d-flex justify-content-start align-items-start'>
+                        <span className='bold'>
+                            311 269 7923</span>
+
+
+                    </div>
+                    <div className='columna-doble d-flex justify-content-start align-items-center'>
+                        <span className='regular'>
+                            dental&smile@gmail.com</span>
+
+                    </div>
+
+
+
+                </div>
+
+                 {/* <div className="col-lg-1">
+                        <h5 className="bold d-flex justify-content-center mb-0 mt-5">Clinica</h5>
+                        <h5 className="d-flex justify-content-center mb-0 mt-5">Dental& healt</h5>
                            
-                                <div className"d-flex flex-row  justify-content-center mb-0 small text-secondary">
-                                <img src={logo} alt="" className"logo" />
+                                <div className="d-flex flex-row  justify-content-center mb-0 small text-secondary">
+                                <img src={logo} alt="" className="logo" />
                                 </div>
                                 
                             
-                        </div> */}
-                    </Row>
-                  
+                </div>  */}
+            </Row>
 
 
-                
-         
+
+
+
         </div>
 
     );
