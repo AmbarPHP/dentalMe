69957a442305c9b8886e30092098008428d5f38c
AmbarPHP <perlamarina.franco@gmail.com>
2023-07-19 14:59:55 -0600

    minimal change , correct spelling in tag

--- src/views/Others/PhotoView2.js
+++ src/views/Others/PhotoView2.js
@@ -4,21 +4,21 @@ import testimonial from "../assets/img/testimonial.png";
 
 export default function PhotoView2() {
     return (
-        <section class="singl-testimonial shadow">
-            <div class="container-fluid py-0">
-                <div class="row align-items-center gradient gradient-primary-dark text-contrast">
-                    <div class="col-md-3 mx-auto py-4 py-md-0">
-                        <p class="badge bg-contrast text-dark shadow rounded-pill py-2 px-4 text-uppercase mb-0">innova tu dentatura</p>
-                        <p class="font-md bold mt-1 text-contrast">Dale confianza y renueva su dentadura</p>
-                        <hr class="my-4"/>
-                            <div class="small text-contrast">
-                                <span class="bold d-block">Mejores precios </span> Promociones en el 2023</div>
-                    </div>
-                    <div class="col-12 col-md-7 my-2 image-background cover" >
-                    <img src={testimonial} className="img-fluid" alt="" />
-                    </div>
-                </div>
-            </div>
-        </section>
+        <section className="singl-testimonial shadow" >
+            <div className="container-fluid py-0" >
+                <div className="row align-items-center gradient gradient-primary-dark text-contrast" >
+                    <div className="col-md-3 mx-auto py-4 py-md-0" >
+                        <p className="badge bg-contrast text-dark shadow rounded-pill py-2 px-4 text-uppercase mb-0" > innova tu dentatura</p >
+                        <p className="font-md bold mt-1 text-contrast" > Dale confianza y renueva su dentadura</p >
+                        <hr className="my-4" />
+                        <div className="small text-contrast" >
+                            <span className="bold d-block" > Mejores precios </span > Promociones en el 2023</div >
+                    </div >
+                    <div className="col-12 col-md-7 my-2 image-background cover" >
+                        <img src={testimonial} className="img-fluid" alt="" />
+                    </div >
+                </div >
+            </div >
+        </section >
     )
 }
