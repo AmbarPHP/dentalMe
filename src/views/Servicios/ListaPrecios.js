4348a6bb59f975a79251551dd007b28c17e67382
AmbarPHP <perlamarina.franco@gmail.com>
2023-07-19 14:50:40 -0600

    minimal fix in lista precios

--- src/views/Servicios/ListaPrecios.js
+++ src/views/Servicios/ListaPrecios.js
@@ -1,6 +1,6 @@
-import { React, useEffect, useState } from 'react';
+import { React } from 'react';
 import data from "./data.json";
-import {Container, Row, Col} from "react-bootstrap"
+import { Row, Col } from "react-bootstrap"
 
 export default function ListaPrecios() {
   return (
@@ -12,12 +12,12 @@ export default function ListaPrecios() {
           data && data.length > 0 && data.map((item) =>
             <Col lg={3} key={item.id} className='bg-primary mx-5  px-5'>
               <h3 className='text-light bold '><span> {item.descripcion} </span></h3>
-                <img style={{height:260+"px", width:"auto"}} src={require('../../assets/img/productos/' + item.picture + '.png')} />
-                
-                <p className='text-light font-20'>Los precios son validos hasta el 1 de septiembre</p>
-                <p className='text-contrast font-20'>Incluye revision y tiene garantia </p>
-                <h2 className='text-light bold' >${item.precio}</h2>
-              
+              <img alt="precios" style={{ height: 260 + "px", width: "auto" }} src={require('../../assets/img/productos/' + item.picture + '.png')} />
+
+              <p className='text-light font-20'>Los precios son validos hasta el 1 de septiembre</p>
+              <p className='text-contrast font-20'>Incluye revision y tiene garantia </p>
+              <h2 className='text-light bold' >${item.precio}</h2>
+
             </Col>
 
           )
