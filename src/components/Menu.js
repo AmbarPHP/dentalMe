3bd74ad0def15ad5e82ffb07b227e2fe6581b9a1
AmbarPHP <perlamarina.franco@gmail.com>
2023-07-19 18:01:44 -0600

    min fix remove import s

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,62 +1,60 @@
-import React, { useState, useRef, useEffect} from 'react';
-
-import Container from 'react-bootstrap/Container';
+import React, { useState, useEffect } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Row, Col } from 'react-bootstrap';
+
 import "./Menu.scss";
 
 
 
 
 export default function Menu() {
-   
-     const [sticky, setSticky]=useState(false);
+
+    const [sticky, setSticky] = useState(false);
 
 
-      useEffect(() => {
+    useEffect(() => {
         const handleScroll = () => {
-            
-            const altura_hero=380;
-            if(window.scrollY > altura_hero){
-                        setSticky(true);
-                      
-                    }
-                    else {
-                        setSticky(false);
-                        
-                    }
+
+            const altura_hero = 380;
+            if (window.scrollY > altura_hero) {
+                setSticky(true);
+
+            }
+            else {
+                setSticky(false);
+
+            }
 
         };
         window.addEventListener('scroll', handleScroll);
-  
-      }, [setSticky]);
 
-      
+    }, [setSticky]);
+
+
 
     return (
 
-            <Navbar  expand="lg"   className={`${sticky ?' fixed1 ':'fixed-before'}`} style={{position:'fixed '}}  >
-               
-                    
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    
-                    <Navbar.Collapse id="basic-navbar-nav" className='margin-20'  >
-                        <Nav   className='font-20 p-1' >
-                            <Nav.Link href="/home"  className='text-light text-uppercase'>Inicio</Nav.Link>
-                            <Nav.Link href="/servicios" className='text-light text-uppercase'>Servicios</Nav.Link>
-                            <Nav.Link href="/about" className='text-light text-uppercase'>Nuestros Profesionales</Nav.Link>
-                            <Nav.Link href="/ubicacion" className='text-light text-uppercase'>Ubicanos</Nav.Link>
-                            <Nav.Link href="/testimoniales" className='text-light text-uppercase'>testimoniales</Nav.Link>
-
-                        </Nav>
-                    </Navbar.Collapse>
-                
-    
-            </Navbar>
+        <Navbar expand="lg" className={`${sticky ? ' fixed1 ' : 'fixed-before'}`}
+            style={{ position: 'fixed ' }}  >
 
 
-       
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+            <Navbar.Collapse id="basic-navbar-nav" className='margin-20'  >
+                <Nav className='font-20 p-1' >
+                    <Nav.Link href="/home" className='text-light text-uppercase'>Inicio</Nav.Link>
+                    <Nav.Link href="/servicios" className='text-light text-uppercase'>Servicios</Nav.Link>
+                    <Nav.Link href="/about" className='text-light text-uppercase'>Nuestros Profesionales</Nav.Link>
+                    <Nav.Link href="/ubicacion" className='text-light text-uppercase'>Ubicanos</Nav.Link>
+                    <Nav.Link href="/testimoniales" className='text-light text-uppercase'>testimoniales</Nav.Link>
+
+                </Nav>
+            </Navbar.Collapse>
+
+
+        </Navbar>
+
+
+
     )
 }
