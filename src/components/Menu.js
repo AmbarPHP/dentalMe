a763a73340bd340ddc1532dfb65b4f11b2b547f8
AmbarPHP <perlamarina.franco@gmail.com>
2023-07-19 18:56:39 -0600

    Move menu to the top

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
 import "./Menu.scss";
 
 
@@ -33,19 +32,19 @@ export default function Menu() {
 
 
     return (
-
-        <Navbar expand="lg" className={`${sticky ? ' fixed1 ' : 'fixed-before'}`}
-            style={{ position: 'fixed ' }}  >
+        // SE DESPLAZA CUANDO EL SCRIOLL AVANZA 380, POR ESO TIENE EL EVENT LISTENER
+        <Navbar expand="lg" className={`${sticky ? 'position_initial' : 'position_fixed'} mt-0 mb-2  `}
+            style={{ position: 'fixed ', maxWidth: "100%" }}  >
 
 
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
+            <p>DENTAL CLINIC</p>
             <Navbar.Collapse id="basic-navbar-nav" className='margin-20'  >
-                <Nav className='font-20 p-1' >
+                <Nav className='font-20 p-4' >
                     <Nav.Link href="/home" className='text-light text-uppercase'>Inicio</Nav.Link>
                     <Nav.Link href="/servicios" className='text-light text-uppercase'>Servicios</Nav.Link>
                     <Nav.Link href="/about" className='text-light text-uppercase'>Nuestros Profesionales</Nav.Link>
-                    <Nav.Link href="/ubicacion" className='text-light text-uppercase'>Ubicanos</Nav.Link>
+                    <Nav.Link href="/ubicacion" className='text-light text-uppercase'>Ubícanos</Nav.Link>
                     <Nav.Link href="/testimoniales" className='text-light text-uppercase'>testimoniales</Nav.Link>
 
                 </Nav>
