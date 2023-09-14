77ca994a65ed7a5af60832c83ac574acb07972f2
AmbarPHP <perlamarina.franco@gmail.com>
2023-09-14 03:40:46 -0700

    arreglando margenes  en section nos ubicamos

--- src/views/Ubicanos/Ubicanos.js
+++ src/views/Ubicanos/Ubicanos.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import { Col } from 'react-bootstrap';
+import React from "react";
+import { Col, Row } from "react-bootstrap";
 import mapa from "../../assets/img/mapa.png";
-import Banner from '../../components/Banner';
-import Footer from '../../components/Footer';
-import AboutUs from './AboutUs';
-
+import Banner from "../../components/Banner";
+import Footer from "../../components/Footer";
+import AboutUs from "./AboutUs";
 
 export default function Ubicanos() {
   return (
-    <div >
-      
-      
-       <Col className='d-flex justify-content-center align-items-center mt-5' lg={12} >
-                    <h3 className="text-primary  bold mb-5"
-                        style={{ fontSize: 3 + "rem" }} >Nos ubicamos aqui</h3>
-                </Col>
-      <img src={mapa}/>
-      
-    
-    </div>
-  )
+    <Row>
+      <Col
+        className="d-flex justify-content-center align-items-center mt-5"
+        lg={12}
+      >
+        <h3 className="text-primary  bold mb-5" style={{ fontSize: 3 + "rem" }}>
+          Nos ubicamos aqui
+        </h3>
+      </Col>
+      <img src={mapa} />
+    </Row>
+  );
 }
