812172aa6eb71566d28c7719ee32a7e2641d50b8
AmbarPHP <perlamarina.franco@gmail.com>
2023-09-18 16:11:29 -0700

    modificando el hero

--- src/views/Home/Hero.js
+++ src/views/Home/Hero.js
@@ -8,7 +8,7 @@ import logo from "../../assets/img/logo.png";
 export default function Hero() {
   /* Muestra el menu y la barra superior */
   return (
-    <div className="overlay-blue  h-100">
+    <div className="overlay-hero-bg  h-100">
       {/* cambie el tamaño de la imagend de hero en el css */}
 
       <Row>
@@ -17,34 +17,32 @@ export default function Hero() {
       </Row>
       <Row className=" d-lg-flex align-middle mt-5 mb-5 mx-0 ">
         <Col md={{ span: 5, offset: 0 }} className="mt-5" sm={12}>
-          <img
-            src={logo}
-            alt={"logo"}
-            style={{ height: "12rem" }}
-            className="logo"
-          />
-
-          <h1 className="text-secondary ms-auto mx-auto mb-0 bold font-3rem">
-            Health & Smile{" "}
+          <h1 className="text-light ms-auto mx-auto mb-0 bold font-3rem">
+            Health & Smile
           </h1>
-          <span className="text-secondary font-2rem vh-100">Dental Center</span>
-          <h4 className="text-secondary pt-3 mt-5">
+
+          <span className="text-light font-2rem vh-100">Dental Center</span>
+
+          <h4 className="text-dark font-1rem pt-3 mt-5 mb-5">
             <p>Clínica dental, realizamos</p>
             <p>odontología integral,</p>
             <p>cirugías, implantes ortodoncia y prótesis</p>
           </h4>
-          <Button className="btn_round  mt-4">
-            <a
-              className="text-light bold font-2rem text-uppercase"
-              href="make-an-appointment.html"
-            >
-              <span className="me-2"></span>311 269 79 23
-            </a>
-          </Button>
+          <div className=" mt-5 mb-5 mx-5">
+            <Button>
+              <a
+                className="text-light bold font-1rem text-uppercase"
+                href="make-an-appointment.html"
+              >
+                <span className="me-2"></span>311 269 79 23
+              </a>
+            </Button>
+          </div>
         </Col>
-        <Col md={{ span: 5, offset: 1 }}>
+
+        {/* <Col md={{ span: 5, offset: 1 }} className="mt-5" sm={12}>
           <div className="gb-primary-img-resina2"></div>
-        </Col>
+        </Col> */}
       </Row>
     </div>
   );
