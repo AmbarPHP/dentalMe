0a100418e912d602620fde148425d38ec4fd8772
AmbarPHP <perlamarina.franco@gmail.com>
2023-09-18 06:51:21 -0700

    add style  to the menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,11 +4,12 @@ import Navbar from "react-bootstrap/Navbar";
 import "./Menu.scss";
 
 export default function Menu() {
-  const [sticky, setSticky] = useState(false);
+  const [sticky, setSticky] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
-      const altura_hero = 380;
+      // a l atura de 38 cambia d posisicion el menu
+      const altura_hero = 38;
       if (window.scrollY > altura_hero) {
         setSticky(true);
       } else {
@@ -23,27 +24,27 @@ export default function Menu() {
     <Navbar
       expand="lg"
       className={`menu-container  ${
-        sticky ? "position_initial" : "position_fixed"
+        sticky ? "position_fixed" : "position_initial"
       } mt-4 mb-2  `}
       style={{ position: "fixed ", maxWidth: "100%" }}
     >
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <p className="text-white">Clínica Dental</p>
+      {/* <p className="title-menu">Clínica Dental</p> */}
       <Navbar.Collapse id="basic-navbar-nav" className="margin-20">
-        <Nav className="font-20 p-4">
-          <Nav.Link href="/home" className="text-light text-uppercase">
+        <Nav className="font-20 p-4 menu-colapsible">
+          <Nav.Link href="/home" className=" text-uppercase">
             Inicio
           </Nav.Link>
-          <Nav.Link href="/servicios" className="text-light text-uppercase">
+          <Nav.Link href="/servicios" className=" text-uppercase">
             Servicios
           </Nav.Link>
-          <Nav.Link href="/about" className="text-light text-uppercase">
+          <Nav.Link href="/about" className=" text-uppercase">
             Nuestros Profesionales
           </Nav.Link>
-          <Nav.Link href="/ubicacion" className="text-light text-uppercase">
+          <Nav.Link href="/ubicacion" className=" text-uppercase">
             Ubícanos
           </Nav.Link>
-          <Nav.Link href="/testimoniales" className="text-light text-uppercase">
+          <Nav.Link href="/testimoniales" className=" text-uppercase">
             testimoniales
           </Nav.Link>
         </Nav>
