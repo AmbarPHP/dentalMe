04eaa08c32123207bc0d2e278187a07db251c2d8
AmbarPHP <perlamarina.franco@gmail.com>
2023-09-18 16:13:22 -0700

    cambiando un poco el estilo

--- src/components/BarraSupDesktop.jsx
+++ src/components/BarraSupDesktop.jsx
@@ -8,41 +8,45 @@ export default function     BarraSupDesktop() {
     return (
         <div name="div_container_desk_barra_sup" className="sup-area" index="999" >
             <Row className='desktop w-100 ' >
-                <Col lg={{ span: 1, offset: 1 }} >
-                    {/* <img src={logo} alt={"logo"} style={{ height:"1rem"}} className="logo" /> */}
-                    {/* <span className='text-primary'> Health&Smile</span> */}
-                </Col>
+                
                 <Col lg={{ span: 2, offset: 1 }} className="pb-3 pt-3" >
                     <div className='columna-doble d-flex justify-content-center align-items-center'>
-                        <a href="http://locahost:3000/" style={{ fontSize: 18 + "px" }} className="me-4 text-primary ">
+                        <a href="http://locahost:3000/" style={{ fontSize: 18 + "px" }} className="me-4 text-light ">
                             <FaWhatsapp />
                         </a>
-                        <span className='text-primary'> 311 269 7923</span>
+                        <span className='text-light'> 311 269 7923</span>
                     </div>
                 </Col>
+                
                 <Col lg={{ span: 3, offset: 0 }} className="pb-3 pt-3" >
                     <div className='columna-doble d-flex justify-content-center align-items-center'>
-                        <a href="http://localhost/" style={{ fontSize: 18 + "px" }} className="me-4 text-primary bold">
+                        <a href="http://localhost/" style={{ fontSize: 18 + "px" }} className="me-4 text-light bold">
                             <FaMapMarkerAlt />
                         </a>
-                        <span className='text-primary pe-2'> Av. de la Cultura #94 Int.2 en Plaza Manglar </span>
+                        <span className='text-light pe-2'> Av. de la Cultura #94 Int.2 en Plaza Manglar </span>
                     </div>
                 </Col>
                 <Col lg={{ span: 3, offset: 1 }} md={12} sm={12} style={{ paddingTop: 0 + 'rem' }} >
-                    <div className="d-flex flex-row  justify-content-center mb-0 font-2rem  text-primary">
-                        <a href="https://www.facebook.com/HealthSmileCenter/" className="me-4 bold">
+                    <div className="d-flex flex-row  
+                    justify-content-center mb-0 font-2rem
+                    ">
+                        <a href="https://www.facebook.com/HealthSmileCenter/" className="me-4 bold text-light">
                             <FaFacebook />
                         </a>
-                        <a href="https://www.facebook.com/HealthSmileCenter/" className="me-4  bold">
+                        <a href="https://www.facebook.com/HealthSmileCenter/" className="me-4  bold text-light">
                             <FaTwitter />
                         </a>
-                        <a href="https://www.facebook.com/HealthSmileCenter/" className="me-4  bold ">
+                        <a href="https://www.facebook.com/HealthSmileCenter/" className="me-4  bold text-light">
                             <FaWhatsapp />
                         </a>
-                        <a href="https://www.facebook.com/HealthSmileCenter/" className=" bold ">
+                        <a href="https://www.facebook.com/HealthSmileCenter/" className=" bold text-light">
                             <FaInstagram />
                         </a>
                     </div>
+                </Col>
+                <Col lg={{ span: 1, offset: 1 }} >
+                    <img src={logo} alt={"logo"} style={{ height:"1rem"}} className="logo" />
+                    {/* <span className='text-primary'> Health&Smile</span> */}
                 </Col>
             </Row>
         </div>
