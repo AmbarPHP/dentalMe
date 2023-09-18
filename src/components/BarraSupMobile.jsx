7c10c9511ec0c21be97cd348d21f507035388f1e
AmbarPHP <perlamarina.franco@gmail.com>
2023-09-18 16:13:36 -0700

    cambiando el estilo

--- src/components/BarraSupMobile.jsx
+++ src/components/BarraSupMobile.jsx
@@ -7,29 +7,33 @@ import "./BarraSup.scss";
 export default function BarraSupMobile() {
     return (
         <Container name="div_container_mobile_barra_sup"  >
-            {/* <Row style={{ fontSize: "18px", zIndex: "1" }}>
-                <div className=' d-flex justify-content-start ms-2 align-items-center'>
+            <Row style={{ fontSize: "18px", zIndex: "1" }}>
+                <div className='d-flex justify-content-start  align-items-center'>
+                    {/* <img
+                    src={logo}
+                    alt={"logo"}
+                    style={{ height: "12rem" }}
+                    className="logo"
+                    /> */}
                     <a href="https://www.facebook.com/HealthSmileCenter/"
-                        style={{ fontSize: "38px" }}
-                        className="me-4 text-primary ">
+                        style={{ fontSize: "18px" }}
+                        className="me-4 text-secondary ">
                         <FaWhatsapp />
                     </a>
-                    <span className='text-primary'> 311 269 7923</span>
+                    <span className='text-secondary'> 311 269 7923</span>
                 </div>
 
             </Row>
-            <Row >
-
-                <div className='d-flex justify-content-start mb-5 align-items-center'>
+            <Row style={{ fontSize: "18px", zIndex: "1" }}>
+                <div className='d-flex justify-content-start  align-items-center'>
                     <a href="https://www.facebook.com/HealthSmileCenter/"
-                        style={{ fontSize: "38px" }}
-                        className="me-4 text-primary ">
+                        style={{ fontSize: "18px" }}
+                        className="me-4 text-secondary ">
                         <FaMapMarkerAlt />
                     </a>
-                    <span className='text-primary pe-3'> Av. de la Cultura #94 Int.2 en Plaza Manglar </span>
+                    <span className='text-secondary pe-3'> Av. de la Cultura #94 Int.2 en Plaza Manglar </span>
                 </div>
-
-            </Row> */}
+            </Row>
         </Container >
     );
 }
