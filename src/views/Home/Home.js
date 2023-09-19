21474d1cd418bb6cbc5d972e83eb71f70ca3f656
AmbarPHP <perlamarina.franco@gmail.com>
2023-09-19 08:11:58 -0700

    change home page sort of content

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import Hero from './Hero';
+import React from "react";
+import Hero from "./Hero";
 
+import Section1 from "../Servicios/Section1";
+import Section4 from "./Section4";
+import Section5 from "./Section5";
+import Section6 from "./Section6";
+import Footer from "../../components/Footer";
+import Nosotros from "../Conocemos/Nosotros";
 
-import Section1 from '../Servicios/Section1';
-import Section4 from './Section4';
-import Section5 from './Section5';
-import Section6 from './Section6';
-import Footer from '../../components/Footer';
-import Nosotros from '../Conocemos/Nosotros';
-
-import Ubicanos from '../Ubicanos/Ubicanos';
+import Ubicanos from "../Ubicanos/Ubicanos";
 import "../Home/Home.scss";
-import Testimonial from '../Testimonials/Testimonial';
+import Testimonial from "../Testimonials/Testimonial";
 
 export default function Home() {
   return (
@@ -20,21 +19,21 @@ export default function Home() {
       <Hero></Hero>
       {/* aqui las secciones seran los componentes en la carpeta home */}
       {/* <Section2></Section2> */}
-      {/* ==========Paquetes==================== */}
-      <Section1></Section1>
+
       {/* ==========Nuestros Servicios=============== */}
       <Section4></Section4>
-      {/* ==========Banner Resina==================== */}
-      <Section6></Section6>
+      {/* ==========Paquetes==================== */}
+      <Section1></Section1>
 
       {/* ===========Nosotros=========================*/}
       <Nosotros></Nosotros>
+      {/* ==========Banner Resina==================== */}
+      <Section6></Section6>
       {/* ==========Dignosticos rapidos==================== */}
       <Section5></Section5>
       <Testimonial></Testimonial>
       <Ubicanos></Ubicanos>
       <Footer></Footer>
-
     </div>
   );
 }
