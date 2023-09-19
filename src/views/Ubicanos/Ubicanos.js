f5f712c58f3d891cc590236c007e0d16f0e293c1
AmbarPHP <perlamarina.franco@gmail.com>
2023-09-18 17:20:06 -0700

    quick fix

--- src/views/Ubicanos/Ubicanos.js
+++ src/views/Ubicanos/Ubicanos.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import mapa from "../../assets/img/mapa.png";
 import Banner from "../../components/Banner";
 import Footer from "../../components/Footer";
@@ -7,16 +7,21 @@ import AboutUs from "./AboutUs";
 
 export default function Ubicanos() {
   return (
-    <Row>
-      <Col
-        className="d-flex justify-content-center align-items-center mt-5"
-        lg={12}
-      >
-        <h3 className="text-primary  bold mb-5" style={{ fontSize: 3 + "rem" }}>
-          Nos ubicamos aqui
-        </h3>
-      </Col>
-      <img src={mapa} />
-    </Row>
+    <Container
+      className="d-flex justify-content-center align-items-center mt-5"
+      lg={12}
+    >
+      <Row>
+        <Col>
+          <h3
+            className="text-primary  bold mb-5"
+            style={{ fontSize: 3 + "rem" }}
+          >
+            Nos ubicamos aqui
+          </h3>
+        </Col>
+        <img alt="mapa" src={mapa} />
+      </Row>
+    </Container>
   );
 }
