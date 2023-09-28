bde7ef31bc726368acc405bb9a2922c7359922d7
AmbarPHP <perlamarina.franco@gmail.com>
2023-09-28 15:49:25 -0700

    Add class desktop

--- src/views/Home/Section5.js
+++ src/views/Home/Section5.js
@@ -4,13 +4,13 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 export default function Section5() {
   return (
     <Container name="section5-home" className="d-flex  container-fluid  ">
-      <Row className="justify-content-lg-left  hero-container gb-primary-img-resina1">
-        <div className=" h-100">
-          <Col
-            md={{ span: 5, offset: 1 }}
-            style={{ paddingTop: 10 + "rem" }}
-            sm={12}
-          >
+      <Row className="justify-content-lg-left  gb-primary-img-resina1">
+        <Col
+          md={{ span: 5, offset: 1 }}
+          style={{ paddingTop: 10 + "rem" }}
+          sm={12}
+        >
+          <div className="desktop">
             <h1 style={{ fontSize: 4 + "rem" }} className="text-light">
               Diagnosticos rapidos y eficientes
             </h1>
@@ -18,17 +18,42 @@ export default function Section5() {
               Le ofrecemos el mas amplio rango de diagnostico,desde Rayos X
               hasta PruebasX.
             </h4>
+          </div>
+          <div class="mobile ">
+            <div className="position-right-1">
+              <h1 style={{ fontSize: 3 + "rem" }} className="  bold text-dark">
+                3
+              </h1>
+            </div>
+            <div className="position-right">
+              <h4 className=" text-dark">
+                Razones para agendar tu cita en Healt & Smile
+              </h4>
+            </div>
+            <div className="position-down">
+              <h4 className=" text-white" style={{ fontSize: 0.8 + "rem" }}>
+                Modernas instalaciones y tecnología para realizar todas tus
+                evaluaciones.
+              </h4>
+              <h4 className=" text-white" style={{ fontSize: 0.8 + "rem" }}>
+                Nuestra proridad es tu tranquilidad, con nosotros no temeras en
+                venir al dentista.
+              </h4>
+              <h4 className=" text-white" style={{ fontSize: 0.8 + "rem" }}>
+                Somos una clinica que te ofrece bienestar y estetica de calidad.
+              </h4>
+            </div>
 
-            <Button variant="light" className="mt-3">
+            {/* <Button variant="light" className="mt-3">
               <a
-                className="btn btn-ellipse btn-white offset-top-30"
+                className="btn btn-ellipse btn-white offset-top-60"
                 href="/opoitment"
               >
                 Diagnosticos Gratis
               </a>
-            </Button>
-          </Col>
-        </div>
+            </Button> */}
+          </div>
+        </Col>
       </Row>
     </Container>
   );
