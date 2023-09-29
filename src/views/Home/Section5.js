53b47b3a925e52d6ee2eeae8a02a1b67b46f265b
AmbarPHP <perlamarina.franco@gmail.com>
2023-09-29 15:11:18 -0700

    Modificando la section 5 y section 6 para mobile

--- src/views/Home/Section5.js
+++ src/views/Home/Section5.js
@@ -3,48 +3,62 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 // import Resina from "../../assets/img/carrusel/carrusel4.png"
 export default function Section5() {
   return (
-    <Container name="section5-home" className="d-flex  container-fluid  ">
-      <Row className="justify-content-lg-left  gb-primary-img-resina1">
+    // anters tenia este background gb-primary-img-resina1
+    <Container id="section5-home" className="d-flex  container-fluid  ">
+      {/* <Row className="justify-content-lg-left  ">
         <Col
           md={{ span: 5, offset: 1 }}
           style={{ paddingTop: 10 + "rem" }}
           sm={12}
-        >
-          <div className="desktop">
-            <h1 style={{ fontSize: 4 + "rem" }} className="text-light">
-              Diagnosticos rapidos y eficientes
-            </h1>
-            <h4 className=" text-white">
-              Le ofrecemos el mas amplio rango de diagnostico,desde Rayos X
-              hasta PruebasX.
-            </h4>
-          </div>
-          <div class="mobile ">
-            <div className="position-right-1">
-              <h1 style={{ fontSize: 3 + "rem" }} className="  bold text-dark">
-                3
-              </h1>
-            </div>
-            <div className="position-right">
-              <h4 className=" text-dark">
-                Razones para agendar tu cita en Healt & Smile
-              </h4>
-            </div>
-            <div className="position-down">
-              <h4 className=" text-white" style={{ fontSize: 0.8 + "rem" }}>
-                Modernas instalaciones y tecnología para realizar todas tus
-                evaluaciones.
-              </h4>
-              <h4 className=" text-white" style={{ fontSize: 0.8 + "rem" }}>
-                Nuestra proridad es tu tranquilidad, con nosotros no temeras en
-                venir al dentista.
-              </h4>
-              <h4 className=" text-white" style={{ fontSize: 0.8 + "rem" }}>
-                Somos una clinica que te ofrece bienestar y estetica de calidad.
-              </h4>
-            </div>
+        > */}
+      <div className="desktop">
+        <h1 style={{ fontSize: 4 + "rem" }} className="text-light">
+          Diagnosticos rapidos y eficientes
+        </h1>
+        <h4 className=" text-white">
+          Le ofrecemos el mas amplio rango de diagnostico,desde Rayos X hasta
+          Pruebas Clinicas.
+        </h4>
+      </div>
+      <div class="mobile ">
+        <div className="position-right-1">
+          <h1 style={{ fontSize: 1 + "rem" }} className="  bold text-dark">
+            Acerca de nuestros servicios
+          </h1>
+          <h4 className=" text-white" style={{ fontSize: 0.8 + "rem" }}>
+            Modernas instalaciones y tecnología para realizar todas tus
+            evaluaciones.
+          </h4>
+          <h4 className=" text-white" style={{ fontSize: 0.8 + "rem" }}>
+            Nuestra proridad es tu tranquilidad, con nosotros no temeras en
+            venir al dentista.
+          </h4>
+          <h4 className=" text-white" style={{ fontSize: 0.8 + "rem" }}>
+            Somos una clinica que te ofrece bienestar y estetica de calidad.
+          </h4>
+        </div>
+        <div className="position-left">
+          <h4 className=" text-white bold">
+            {/* Razones por las que agendar con dental  */}
+            Tu salud es nuestra proridad en Healt & Smile
+          </h4>
+        </div>
+        <div className="position-left-down">
+          <h4 className="text-uppercase text-white">Los mejores dentistas</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa earum
+            ex vero, tempore dicta veritatis, sed soluta adipisci.
+          </p>
+        </div>
+        <div className="position-left-down-2">
+          <h4 className="text-uppercase text-white">Tecnologia Dental</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa earum
+            ex vero.
+          </p>
+        </div>
 
-            {/* <Button variant="light" className="mt-3">
+        {/* <Button variant="light" className="mt-3">
               <a
                 className="btn btn-ellipse btn-white offset-top-60"
                 href="/opoitment"
@@ -52,9 +66,9 @@ export default function Section5() {
                 Diagnosticos Gratis
               </a>
             </Button> */}
-          </div>
-        </Col>
-      </Row>
+      </div>
+      {/* </Col>
+      </Row> */}
     </Container>
   );
 }
--- src/views/Home/Section6.js
+++ src/views/Home/Section6.js
@@ -5,41 +5,44 @@ import "../../App.scss";
 export default function Section6() {
   return (
     <Container name="section6-home" className="my-5 bg-secondary ">
-      <Row className=" d-flex  align-items-start  mb-5">
-        <Col lg={6} sm={12} className="mt-5">
-          <h1
-            className="text-title-mobile text-title-desktop text-border-round-light
+      <div id="sectionSection6-home">
+        <Row className=" d-flex  align-items-start  mb-5">
+          <Col lg={6} sm={12} className="mt-5">
+            <h1
+              className="text-title-mobile text-title-desktop text-border-round-light
            font-3rem  mb-3 px-5"
-          >
-            Beneficios de la <span>resina dental</span>
-          </h1>
+            >
+              Beneficios de la <span>resina dental</span>
+            </h1>
 
-          {/* text-border-round-primary text-light */}
+            {/* text-border-round-primary text-light */}
 
-          <h4 className=" mt-5 px-5" style={{ color: "$secondary" }}>
-            Se adhiere a la superficie del diente.
-          </h4>
-          <h4 className="  px-5 " style={{ color: "$secondary" }}>
-            Respeta la estetica dental.
-          </h4>
-          <h4 className=" px-5" style={{ color: "$secondary" }}>
-            Mantiene una apariencia natural
-          </h4>
-          <h4 className="px-5" style={{ color: "$secondary" }}>
-            Es practicamente inperceptible
-          </h4>
+            <h4 className=" mt-5 px-5" style={{ color: "$secondary" }}>
+              Se adhiere a la superficie del diente.
+            </h4>
+            <h4 className="  px-5 " style={{ color: "$secondary" }}>
+              Respeta la estetica dental.
+            </h4>
+            <h4 className=" px-5" style={{ color: "$secondary" }}>
+              Mantiene una apariencia natural
+            </h4>
+            <h4 className="px-5" style={{ color: "$secondary" }}>
+              Es practicamente inperceptible
+            </h4>
 
-          <Button className="mx-5 my-5 px-5 ">
-            <a className="btn-primary text-secondary " href="/opoitment ">
-              Haz una cita
-            </a>
-          </Button>
-        </Col>
-        <Col lg={6} sm={12} className="bg-primary  py-5 px-5 my-5">
-          <h1 className="bold text-light font-3rem"> 50% de descuento</h1>
-          <p className="text-light">Aplicable: 25 Feb al 11 de Marzo</p>
-        </Col>
-      </Row>
+            <Button className="mx-5 my-5 px-5 ">
+              <a className="btn-primary text-secondary " href="/opoitment ">
+                Haz una cita
+              </a>
+            </Button>
+          </Col>
+          <Col lg={6} sm={12} className="bg-primary  py-5 px-5 my-5">
+            <h1 className="bold text-light font-3rem"> 50% de descuento</h1>
+            <p className="text-light">Aplicable: 25 Feb al 11 de Marzo</p>
+          </Col>
+        </Row>
+      </div>
+      <div id="sectionSection6-mobile"></div>
     </Container>
   );
 }
