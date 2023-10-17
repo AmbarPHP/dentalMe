38f07a08fd35ecdd34aa7137c94075ad81ad864a
AmbarPHP <perlamarina.franco@gmail.com>
2023-10-17 14:50:04 -0700

    cambiando el mapa

--- src/views/Ubicanos/Ubicanos.js
+++ src/views/Ubicanos/Ubicanos.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import mapa from "../../assets/img/mapa.png";
+import mapa from "../../assets/img/mapa1.png";
 
 export default function Ubicanos() {
   return (
