b7740f6769961bd32ef2a29e57f8088a83fb9a5c
AmbarPHP <perlamarina.franco@gmail.com>
2023-10-31 14:58:47 -0700

    corrigiendo menu fixed

--- src/components/MenuDesktop.js
+++ src/components/MenuDesktop.js
@@ -3,9 +3,10 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./Menu.scss";
 import logo from "../assets/img/logo.png";
+import { Col } from "react-bootstrap";
 
 export default function MenuDesktop() {
-  const [sticky, setSticky] = useState(true);
+  const [sticky, setSticky] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -22,38 +23,38 @@ export default function MenuDesktop() {
 
   return (
     // SE DESPLAZA CUANDO EL SCRIOLL AVANZA 380, POR ESO TIENE EL EVENT LISTENER
-
-    <Navbar
-      expand="sm"
-      className={`menu-container_desktop  ${
-        sticky ? "position_fixed" : "position_initial"
-      } mt-4 mb-4  `}
-    >
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Brand></Navbar.Brand>
-      <Navbar.Collapse
-        id="basic-navbar-nav"
-        // style={{ backgroundColor: "#f33" }}
+    <Col className="lg-12 md-12">
+      <Navbar
+        expand="sm"
+        className={`menu-container_desktop  ${
+          sticky ? "position_fixed" : "position_initial"
+        } mt-4 mb-4  `}
       >
-        <Nav className="font-18 p-4 menu-colapsible">
-          <Nav.Link href="/home" className=" text-uppercase">
-            Inicio
-          </Nav.Link>
-          <Nav.Link href="/servicios" className=" text-uppercase">
-            Servicios
-          </Nav.Link>
-          <Nav.Link href="/about" className=" text-uppercase">
-            Nuestros Profesionales
-          </Nav.Link>
-          <Nav.Link href="/ubicacion" className=" text-uppercase">
-            Ubícanos
-          </Nav.Link>
-          <Nav.Link href="/testimoniales" className=" text-uppercase">
-            Testimoniales
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-    // </div>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand></Navbar.Brand>
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          //style={{ backgroundColor: "#f33" }}
+        >
+          <Nav className="font-18 p-4 menu-colapsible">
+            <Nav.Link href="/home" className=" text-uppercase">
+              Inicio
+            </Nav.Link>
+            <Nav.Link href="/servicios" className=" text-uppercase">
+              Servicios
+            </Nav.Link>
+            <Nav.Link href="/about" className=" text-uppercase">
+              Nuestros Profesionales
+            </Nav.Link>
+            <Nav.Link href="/ubicacion" className=" text-uppercase">
+              Ubícanos
+            </Nav.Link>
+            <Nav.Link href="/testimoniales" className=" text-uppercase">
+              Testimoniales
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Col>
   );
 }
